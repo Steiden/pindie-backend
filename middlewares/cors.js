62a38e0c55b188f58b091837791fc07d45d7a5ca
Daniil <baldindaniilza@mail.ru>
2024-05-28 22:59:44 +0500

    Fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,28 +1,18 @@
-// Список сайтов, которым мы разрешаем обращаться к серверу
-const allowedCors = [
-	"https://practicum.yandex.ru",
-	"https://students-projects.ru",
-	"http://localhost:3000",
-	"http://localhost:3001",
-	"https://pindie-client.nomorepartiesco.ru",
-];
+// Вместо нашего адреса вставь свой
+// Можно ещё не удалять адреса с localhost, но мы удалили для краткости
+const allowedCors = ["https://pindie-client.nomorepartiesco.ru"];
 
-// Функция, которая принимает объекты req (информация о запросе),
-// res (объект ответа) и функцию next (для запуска следующего миддлвара)
 function cors(req, res, next) {
 	const { origin } = req.headers;
 
 	if (allowedCors.includes(origin)) {
-		// Если это наш друг
 		res.header("Access-Control-Allow-Origin", origin);
 	}
-
 	res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
 	res.header(
 		"Access-Control-Allow-Headers",
 		"Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
 	);
-
 	next();
 }
 
